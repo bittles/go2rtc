6745317b054a347099a6ab87d198a5ae4600b932
bittles <bauern.sc@gmail.com>
2026-02-19 03:05:33 -0600

    Revert "change logging"
    This reverts commit 894343690f3ce5e25de6b925894b4319ced6be2b.

--- pkg/h264/rtp.go
+++ pkg/h264/rtp.go
@@ -1,7 +1,7 @@
 package h264
 
 import (
-	"log"
+	"fmt"
 	"encoding/binary"
 
 	"github.com/AlexxIT/go2rtc/pkg/core"
@@ -28,8 +28,7 @@ func RTPDepay(codec *core.Codec, handler core.HandlerFunc) core.HandlerFunc {
 
 	// Patch level in SDP SPS
 	// outputting fmptline to log
-	log.Printf("[RTP] Fmtpline: %s", codec.FmtpLine)
-	//fmt.Println(codec.FmtpLine)
+	fmt.Println(codec.FmtpLine)
 	if len(sps) >= 4 {
 			sps[3] = 0x29 // Level 4.1
 	}
