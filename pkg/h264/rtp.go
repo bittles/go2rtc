561ff53c53db331fe80578cbf6e3a1c5354d4530
bittles <bauern.sc@gmail.com>
2026-02-19 03:05:29 -0600

    Revert "Update rtp.go"
    This reverts commit 632a23906444aa1adfc25e06c47228d16753390a.

--- pkg/h264/rtp.go
+++ pkg/h264/rtp.go
@@ -28,8 +28,7 @@ func RTPDepay(codec *core.Codec, handler core.HandlerFunc) core.HandlerFunc {
 
 	// Patch level in SDP SPS
 	// outputting fmptline to log
-	//log.Printf("[RTP] Fmtpline: %s", codec.FmtpLine)
-	log.Info().Str("fmtpline", codec.FmtpLine).Msg("[rtp] ouput from FmtpLine")
+	log.Printf("[RTP] Fmtpline: %s", codec.FmtpLine)
 	//fmt.Println(codec.FmtpLine)
 	if len(sps) >= 4 {
 			sps[3] = 0x29 // Level 4.1
